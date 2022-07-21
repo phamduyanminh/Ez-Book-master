5b939d70cd3282414642fe52a6b080d37e09e822
Minh Pham <43801761+phamduyanminh@users.noreply.github.com>
2022-07-21 15:19:05 +0700

    Fix overflowed pixel issue

--- lib/src/pages/books/books_home.dart
+++ lib/src/pages/books/books_home.dart
@@ -48,67 +48,72 @@ class _BooksHomeState extends State<BooksHome> {
                               height: 120,
                               child: Row(
                                 children: <Widget>[
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.circular(15),
-                                    child: Hero(
-                                      tag: (state.bookModel.results
-                                          as dynamic)[index],
-                                      child: SizedBox(
-                                        height: 100,
-                                        child: Image(
-                                          fit: BoxFit.cover,
-                                          width: 90,
-                                          image: NetworkImage(
-                                            (state.bookModel.results
-                                                    as dynamic)[index]
-                                                .imgUrl
-                                                .toString(),
+                                  Expanded(
+                                    flex: 0,
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(15),
+                                      child: Hero(
+                                        tag: (state.bookModel.results
+                                            as dynamic)[index],
+                                        child: SizedBox(
+                                          height: 100,
+                                          child: Image(
+                                            fit: BoxFit.cover,
+                                            width: 90,
+                                            image: NetworkImage(
+                                              (state.bookModel.results
+                                                      as dynamic)[index]
+                                                  .imgUrl
+                                                  .toString(),
+                                            ),
                                           ),
                                         ),
                                       ),
                                     ),
                                   ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(20),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          (state.bookModel.results
-                                                  as dynamic)[index]
-                                              .name
-                                              .toString(),
-                                          overflow: TextOverflow.ellipsis,
-                                          style: const TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 16,
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(15),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            (state.bookModel.results
+                                                    as dynamic)[index]
+                                                .name
+                                                .toString(),
+                                            overflow: TextOverflow.ellipsis,
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16,
+                                            ),
                                           ),
-                                        ),
-                                        const SizedBox(height: 5),
-                                        Text(
-                                          (state.bookModel.results
-                                                  as dynamic)[index]
-                                              .desc
-                                              .toString(),
-                                          style: const TextStyle(
-                                              color: Colors.grey),
-                                        ),
-                                        const SizedBox(height: 10),
-                                        Text(
-                                          (state.bookModel.results
-                                                  as dynamic)[index]
-                                              .type
-                                              .join(", "),
-                                          maxLines: 1,
-                                          overflow: TextOverflow.clip,
-                                          softWrap: true,
-                                          style: const TextStyle(
-                                            color: Colors.grey,
-                                            fontSize: 14,
+                                          const SizedBox(height: 5),
+                                          Text(
+                                            (state.bookModel.results
+                                                    as dynamic)[index]
+                                                .desc
+                                                .toString(),
+                                            style: const TextStyle(
+                                                color: Colors.grey),
                                           ),
-                                        )
-                                      ],
+                                          const SizedBox(height: 15),
+                                          Text(
+                                            (state.bookModel.results
+                                                    as dynamic)[index]
+                                                .type
+                                                .join(", "),
+                                            maxLines: 1,
+                                            overflow: TextOverflow.clip,
+                                            softWrap: true,
+                                            style: const TextStyle(
+                                              color: Colors.grey,
+                                              fontSize: 14,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
                                     ),
                                   ),
                                 ],
