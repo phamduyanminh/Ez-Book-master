5b4ed56c904edb63e8fc0a6e22a956b541fa1bcc
Minh Pham <43801761+phamduyanminh@users.noreply.github.com>
2022-07-27 11:28:54 +0700

    Create user model

--- lib/src/models/user.dart
+++ lib/src/models/user.dart
@@ -1,22 +1,63 @@
 class User {
+  List<Results>? results;
+  User({this.results});
+
+  User.fromJson(Map<String, dynamic> json) {
+    if (json['results'] != null) {
+      results = <Results>[];
+      json['results'].forEach((v) {
+        results!.add(Results.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (results != null) {
+      data['results'] = results!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Results {
+  String? objectId;
   String? username;
   String? name;
   String? password;
   String? phone;
+  String? createdAt;
+  String? updatedAt;
 
-  User({
+  Results({
+    this.objectId,
     this.username,
     this.name,
     this.password,
     this.phone,
+    this.createdAt,
+    this.updatedAt,
   });
 
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      username: json['username'] as String,
-      name: json['name'] as String,
-      password: json['password'] as String,
-      phone: json['phone'] as String,
-    );
+  Results.fromJson(Map<String, dynamic> json) {
+    objectId = json['objectId'];
+    username = json['username'];
+    name = json['name'];
+    password = json['password'];
+    phone = json['phone'];
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['objectId'] = objectId;
+    data['username'] = username;
+    data['name'] = name;
+    data['password'] = password;
+    data['phone'] = phone;
+    data['createdAt'] = createdAt;
+    data['updatedAt'] = updatedAt;
+    return data;
   }
 }
