a0e99bd1b5be9116e7f141a14fbde50f997f6846
Minh Pham <43801761+phamduyanminh@users.noreply.github.com>
2022-07-28 13:54:13 +0700

    Indicate blocs of authentication

--- lib/src/blocs/authbloc/bloc/authentication_bloc.dart
+++ lib/src/blocs/authbloc/bloc/authentication_bloc.dart
@@ -12,8 +12,28 @@ class AuthenticationBloc
 
   AuthenticationBloc({required this.userRepository})
       : super(AuthenticationUninitialized()) {
-    on<AuthenticationEvent>((event, emit) {
-      // TODO: implement event handler
+    // ignore: void_checks
+    on<AuthenticationEvent>((event, emit) async* {
+      if (event is AppStarted) {
+        final bool hasToken = await userRepository.hasToken();
+        if (hasToken) {
+          yield AuthenticationAuthenticated();
+        } else {
+          yield AuthenticationUnauthenticated();
+        }
+      }
+
+      if (event is LoggedIn) {
+        yield AuthenticationLoading();
+        await userRepository.persistToken(event.token);
+        yield AuthenticationAuthenticated();
+      }
+
+      if (event is LoggedOut) {
+        yield AuthenticationLoading();
+        await userRepository.deleteToken();
+        yield AuthenticationUnauthenticated();
+      }
     });
   }
 }
